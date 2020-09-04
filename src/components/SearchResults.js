b4c198522bc39eac5a758fd9e7d709986efd6cdb
Brittny Rosenow <brittnyrosenow@gmail.com>
2020-09-04 11:55:06 -0600

    redeploy

---
+++ src/components/SearchResults.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+
+import API from "../src/utils/api";
+import EmployeeCard from "./EmployeeCard";
+// import ResultList from "./ResultList";
+
+export default class SearchResults extends Component {
+
+
+  componentDidMount() {
+    API.searchEmployees().then(res => {
+        console.log(res)
+        this.setState({
+          result: res.data.results.map((e, i) => ({
+            picture: e.picture.medium,
+            firstName: e.name.first,
+            lastName: e.name.last,
+            phone: e.phone,
+            email: e.email,
+            dog: e.dob.date,
+            key: i
+          }))  
+        })
+    })
+    .catch(err => console.log(err));
+  }
+
+  // searchEmployees = query => {
+  //   API.search(query)
+  //   console.log(res)
+    
+  //     .then(res => this.setState({ result: res.data }))
+  //     .catch(err => console.log(err));
+  // };
+
+  handleInputChange = event => {
+    const value = event.target.value;
+    const name = event.target.name;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  
+  handleFormSubmit = event => {
+    event.preventDefault();
+    this.searchEmployees(this.state.search);
+  };
+
+render() {
+  return (<div>
+    <h1>Hi {this.state.result[0].firstName}</h1>
+  </div>)
+}
+
+ }
+
